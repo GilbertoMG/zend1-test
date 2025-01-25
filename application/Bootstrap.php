e672284bcff1b5086ceeb6b349b7be930c7e60c3
GilbertoMG <gilbmg@gmail.com>
2025-01-25 17:31:40 -0300

    Testando a criação de um formulário com o Zend Form

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,18 +1,4 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{protected function _initViewHelpers()
-	{
-		// // Cria uma instância de Zend_View
-		// $view = new Zend_View();
-	
-		// // Adiciona o caminho padrão dos helpers do Zend Framework
-		// $view->addHelperPath('Zend/View/Helper', 'Zend_View_Helper');
-	
-		// // Adiciona o caminho para helpers personalizados
-		// $view->addHelperPath(APPLICATION_PATH . '/views/helpers', 'Application_View_Helper');
-	
-		// // Configura o view renderer para usar essa view
-		// $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
-		// $viewRenderer->setView($view);
-	}
+{
 }
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,6 +8,23 @@ class IndexController extends Zend_Controller_Action
     
     public function indexAction()
     {
+        $form = new Zend_Form();
+
+        $form->setAttrib('id', 'formIndex')
+             ->setAction('/index/buscar')
+             ->setMethod('post')
+             ->addElement('text', 'nome', array(
+                 'label' => 'Nome',
+                 'required' => true,
+                 'filters' => array('StringTrim'),
+                 'validators' => array('NotEmpty')
+             ))
+             ->addElement('submit', 'buscar', array(
+                 'label' => 'Buscar'
+             ));
+
+        $this->view->form = $form;
+
         $this->view->teste = " teste da view";
     }
 
