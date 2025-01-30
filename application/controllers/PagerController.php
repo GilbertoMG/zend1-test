0721287b1a13759dbbbfa8ac1807324f938900e0
GilbertoMG <gilbmg@gmail.com>
2025-01-29 23:12:29 -0300

    Testando a criação de um formulário com o Zend Form

--- application/controllers/PagerController.php
+++ application/controllers/PagerController.php
@@ -10,9 +10,10 @@ class PagerController extends Zend_Controller_Action
     
     public function indexAction()
     {
-        $dados = range(1, 50); // Array de 1 a 50
+        $dados = range(1, 200); // Array de 1 a 50
         $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_Array($dados));
         $paginator->setCurrentPageNumber($this->getParam('page')); // Define a página atual como 5
+        $paginator->setPageRange(10);
         $this->view->paginator = $paginator; // Passa o paginator para a view
 
     }
